ac55e27f8c7708f88f96bdd6b857e9b5cad398eb
Aaron de Boer <aarondeboerwk@gmail.com>
2022-09-28 18:02:21 +0200

    Made a simple snow boarding level with some basic triggers and rotation movements of the player when pressing left and right arrow keys

---
+++ Assets/Scripts/FinishLine.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a9fcf40aa2b1298c35106817eedfece8b01c5741ec709d8cb028380590702637
+size 296
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3350aa4f91a6098752cbd9055c0db870beb136b06220521d33101d262fc5d8d9
+size 633
